ed48b01d5c471f239e313f3eb7e4727154653723
Njaso <jason749573@163.com>
2024-10-31 19:30:56 +0800

    分数查询接口

--- www.csgamehub.hust.cn/backend/api/game_rank_api.php
+++ www.csgamehub.hust.cn/backend/api/game_rank_api.php
@@ -8,7 +8,7 @@ $rest = [
 
 function error_and_die($msg)
 {
-    global $rest;
+    global $rest; // 函数作用域内无法访问外部变量, 需要加上global来访问
     $rest["code"] = 1;
     $rest["msg"] = $msg;
     echo json_encode($rest);
@@ -22,5 +22,23 @@ $password = $db_setting["password"];
 $dbName = $db_setting["databaseName"];
 $conn = new mysqli($serverName, $username, $password, $dbName);
 if ($conn->connect_error) {
-
+    error_and_die("数据库连接错误");
 }
+
+$gamename = $_POST["gamename"];
+$tablename = "gamescorelist";
+$sql = "
+    select username,highest_score
+    from $tablename
+    where gamename='$gamename'
+    limit 20;
+";
+
+$res = $conn->query($sql);
+if ($res) {} else {
+    $conn->close();
+    error_and_die($conn->error);
+}
+
+$conn->close();
+echo json_encode($rest);
