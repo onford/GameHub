538f42d78390850e41d5c0717283a3b475fc7a01
Njaso <jason749573@163.com>
2024-11-01 15:14:51 +0800

    添加了查询第零层的接口

--- www.csgamehub.hust.cn/backend/api/get_layer0_comment_api..php
+++ www.csgamehub.hust.cn/backend/api/get_layer0_comment_api..php
@@ -23,3 +23,34 @@ $conn = new mysqli($serverName, $username, $password, $dbName);
 if ($conn->connect_error) {
     error_and_die("数据库连接失败");
 }
+
+// 传入参数gamename
+$gamename = $_POST['gamename'];
+$tablename = "comment_list";
+$sql = "
+    select *
+    from $tablename
+    where gamename='$gamename' and root_id is NULL;
+";
+
+$res = $conn->query($sql);
+if ($res) {
+    while ($row = $res->fetch_assoc()) {
+        $new_node = [
+            "id" => $row['id'],
+            "username" => $row['username'],
+            // 'gamename'=>$row['gamename']; 应该不需要?
+            'comment' => $row['comment'],
+            'root_id' => $row['root_id'],
+            'likes' => $row['likes'],
+            'unlikes' => $row['unlikes'],
+        ];
+        array_push($rest["data"], $new_node); // $new_node不能用json_encode转成json格式的对象, js认不出来
+    }
+} else {
+    $conn->close();
+    error_and_die($conn->error);
+}
+
+$conn->close();
+echo json_encode($rest);
