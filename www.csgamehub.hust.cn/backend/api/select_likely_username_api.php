d6e8d3c6632a44e00633c52c6b0a598bb146e65b
Njaso <jason749573@163.com>
2024-11-02 20:15:42 +0800

    添加'查找相似的用户名'接口

---
+++ www.csgamehub.hust.cn/backend/api/select_likely_username_api.php
@@ -0,0 +1,47 @@
+<?php
+$rest = [
+    "code" => 0,
+    "msg" => "",
+    "data" => [],
+];
+
+function error_and_die($msg)
+{
+    global $rest;
+    $rest["code"] = 1;
+    $rest["msg"] = $msg;
+    echo json_encode($rest);
+    die();
+}
+
+// 连接数据库
+$db_setting = require __DIR__ . "./../database/config.php";
+$serverName = $db_setting["serverName"];
+$username = $db_setting["username"];
+$password = $db_setting["password"];
+$dbName = $db_setting["databaseName"];
+$conn = new mysqli($serverName, $username, $password, $dbName);
+if ($conn->connect_error) {
+    error_and_die("数据库连接失败");
+}
+
+$tablename = "userlist";
+$input_text = $_POST["part_username"];
+$sql = "
+    select username
+    from $tablename
+    where username like '%$input_text%';
+";
+$res = $conn->query($sql);
+
+if ($res) {
+    while ($row = $res->fetch_assoc()) {
+        array_push($rest["data"], $row["username"]);
+    }
+} else {
+    $conn->close();
+    error_and_die($conn->error);
+}
+
+$conn->close();
+echo json_encode($rest);
